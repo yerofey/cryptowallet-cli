41533a729633d7479b39fe125365c796db50ff20
Yerofey S <pm@yerofey.dev>
2021-04-23 14:31:51 +0300

    added prefix symbols check

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const bip39 = require('bip39');
 const pkutils = require('ethereum-mnemonic-privatekey-utils');
 const { Account } = require('eth-lib/lib');
 
-const desiredPrefix = (args.length === 1 ? args[0] : '') || '';
+const desiredPrefix = (args.length === 1 ? args[0].toLowerCase() : '') || '';
+// check if prefix match hex format
+const desiredPrefixBadSymbolsArray = desiredPrefix.split('').filter(char => !/[0-9a-f]/g.test(char));
+if (desiredPrefix.length > 0 && desiredPrefixBadSymbolsArray.length > 0) {
+    console.log('Error: prefix contains non-hex characters!');
+    process.exit(1);
+}
 
 while (true) {
     const mnemonic = bip39.generateMnemonic();
