f7bd9c81441cb1f72cad3ad3ef8b29a9eb85794c
yerofey <pm@yerofey.dev>
2024-02-02 11:00:33 +0100

    Update donation addresses for SON and TON

--- src/Method.js
+++ src/Method.js
@@ -397,6 +397,8 @@ class Method {
     - USDT: TCW9eaRWjpivZvnZ5DwgbWxPRpoZNWbuPe
     - BTC: bc1qcwamquntxshqsjcra6vryftrfd9z57j02g3ywq
     - ETH: 0xe3e3ed78d9f8A935a9a0fCE2a7305F2f5DBabAD8
+    - SOL: CWsbNQRxNzAasLd2zfwkEkbBZXKxfoxva14pe8wawUju
+    - TON: UQCWYNjNQdRp7lfmehDWA-RgPqipbI3crX3qFN1MDLpgkyCS
     - DOGE: DMAkWQKx1H6ESG3beDBssn5mAAZcwkrYVh
 
     Donate via PayPal:
